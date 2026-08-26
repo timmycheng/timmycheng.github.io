e10a50c723b5e5bc84aa473974247ab9e5b8ce75
@home <timmycheng@foxmail.com>
2026-08-26 10:17:22 +0800

    fresh start: replace hexo with astro blog template

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Timmy's Blog";
+export const SITE_DESCRIPTION = 'Timmy 的个人博客';
